9a86e105ea8c6d1881f50d7507cd7ec5ac15f4f6
I wanna be real nerd <71207364+IwannabeRealnerD@users.noreply.github.com>
2025-07-27 11:29:32 +0900

    test (#11)

---
+++ src/app/[locale]/lines/section/line-list-section/OttBadge.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from "react";
+
+import { RiMovieLine } from "react-icons/ri";
+import { SiAppletv, SiHbo, SiNetflix } from "react-icons/si";
+import { TbBrandDisney } from "react-icons/tb";
+import { InferOutput } from "valibot";
+
+import { GlobalOttServiceNameSchema } from "@/types/OttServiceName";
+
+interface InternalOttBadgeProps {
+  ottName: InferOutput<typeof GlobalOttServiceNameSchema>;
+}
+
+export const InternalOttBadge: FunctionComponent<InternalOttBadgeProps> = (props) => {
+  switch (props.ottName) {
+    case "HBO max":
+      return <SiHbo className="text-2xl" />;
+    case "Netflix":
+      return <SiNetflix className="text-2xl text-red-500" />;
+    case "Apple TV+":
+      return <SiAppletv className="text-2xl" />;
+    case "Disney Plus":
+      return <TbBrandDisney className="text-2xl text-teal-400" />;
+    case "Movie":
+      return <RiMovieLine className="text-2xl" />;
+    default:
+      return null;
+  }
+};
--- src/utils/globalGetNotionDatabase/index.ts
+++ src/utils/globalGetNotionDatabase/index.ts
@@ -1,30 +1,35 @@
 import { unstable_cache } from "next/cache";
 
+import { GLOBAL_DATABASE_NAME } from "@/constants/databaseName";
 import { GlobalDatabaseName } from "@/types/DatabaseName";
 
 import { internalQueryNotionDatabase } from "./internal/getNotionDatabase";
+/**
+ * @description unstable_cache is used in order to cache the database response and fetchedAt property.
+ * @note this function will be replaced by "use cache" when it becomes stable.
+ * @see https://blog.logrocket.com/caching-next-js-unstable-cache/
+ */
 
-export const globalGetNotionDatabase = (databaseName: GlobalDatabaseName) => {
-  const cachedFunction = unstable_cache(
-    async () =>
-      internalQueryNotionDatabase(databaseName, {
-        filter: {
-          property: "is_done",
-          checkbox: {
-            equals: true,
-          },
+// TODO : Should change the function to accept databaseName as an argument.
+export const globalGetNotionDatabase = unstable_cache(
+  async (databaseName: GlobalDatabaseName) =>
+    internalQueryNotionDatabase(databaseName, {
+      filter: {
+        property: "is_done",
+        checkbox: {
+          equals: true,
         },
-        sorts: [
-          {
-            property: "added_date",
-            direction: "descending",
-          },
-        ],
-      }),
-    [databaseName],
-    {
-      tags: [databaseName],
-    }
-  );
-  return cachedFunction();
-};
+      },
+      sorts: [
+        {
+          property: "added_date",
+          direction: "descending",
+        },
+      ],
+    }),
+  [GLOBAL_DATABASE_NAME.LINES],
+  {
+    tags: [GLOBAL_DATABASE_NAME.LINES],
+    revalidate: 60 * 60 * 24,
+  }
+);
